f48d88eefcb19e6ec6c4cbfecf54b0866903dca8
Milan Mitrovic <milanmitrovic1991@gmail.com>
2024-09-18 13:44:50 +0000

    Resolve "Create pyproject.toml file and folder"

---
+++ src/main.py
@@ -0,0 +1,16 @@
+"""Start Dash application from this file."""
+
+
+import polars as pl
+
+
+def example_function():
+    """
+
+    Return empty DataFrame.
+
+    Function that just returns empty DataFrame.
+
+    :return:
+    """
+    return pl.DataFrame()
